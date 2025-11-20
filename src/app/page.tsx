d9d2522b8f8fd5c863352846f2f1a156cc8186e5
Gabriel Akpanudom <52882295+gmeloddy@users.noreply.github.com>
2025-11-20 20:10:57 +0100

    Change paragraph font size to 15px
    Updated paragraph font size from 17px to 15px.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -202,7 +202,7 @@ const Page = () => {
               HUMTRAN
             </h1>
 
-            <p className="relative w-fit font-work font-normal italic text-[#170902] text-[17px] tracking-[0] leading-none">
+            <p className="relative w-fit font-work font-normal italic text-[#170902] text-[15px] tracking-[0] leading-none">
               Empowering young minds &amp; driving transformation.
             </p>
           </div>
